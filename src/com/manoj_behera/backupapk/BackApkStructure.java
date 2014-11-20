73c4b9cd8c302a53ea1a2892008cf7dd40ae688c
Manoj Behera <mnj.behera1@gmail.com>
2014-11-20 18:28:57 +0530

    Backup APK

---
+++ src/com/manoj_behera/backupapk/BackApkStructure.java
@@ -0,0 +1,13 @@
+package com.manoj_behera.backupapk;
+
+
+
+import android.graphics.drawable.Drawable;
+
+public class BackApkStructure {
+
+	public Drawable icons;
+	public String apkName;
+	public String apkSourceDir;
+	
+}
